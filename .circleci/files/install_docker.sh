0fbab36f42579ef7bbe0f580b2e046138168f353
James Prince <34577138+zutils@users.noreply.github.com>
2021-03-16 15:04:59 -0400

    Added rolling update

--- .circleci/files/install_docker.sh
+++ .circleci/files/install_docker.sh
@@ -5,12 +5,14 @@ then
 echo Docker Already Installed!
 else 
 # Install Docker
+sudo apt-get update
 sudo apt-get -y install \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg \
-    lsb-release
+    lsb-release \
+    socat
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
--- .circleci/files/roll_update.sh
+++ .circleci/files/roll_update.sh
@@ -1,9 +1,34 @@
 #!/usr/bin/env bash
 
 # Do rolling update!
-dockerpath=jzypo/udacity:hello-world-server
-minikube kubectl -- run hello-world-server --image $dockerpath --port=80
+dockerpath=jzypo/udacity:hello_world_server
+
+if $(minikube kubectl -- get deployments | grep -q "No Resources")
+then
+minikube kubectl -- create deployment hello-world-server --image $dockerpath --port=8000
+else
+echo "Found Pod!"
+fi
+
 minikube kubectl -- get pods
 
-# Run in background - forward port 80 to the server's port 8000
-#nohup minikube kubectl --  port-forward hello-world-server 80:8000
+echo "Rolling update!"
+minikube kubectl -- set image deployments/hello-world-server udacity=jzypo/udacity:latest
+
+# Run socat to forward port 80 to port 8000
+if $(ps -A | grep -q kubectl)
+then
+echo "Kubernetes may already be port forwarding"
+else
+echo "Port forwarding kubernetes"
+nohup minikube kubectl --  port-forward deployment/hello-world-server 8000:8000 &
+fi
+
+# Run socat to forward port 80 to port 8000
+if $(ps -A | grep -q socat)
+then
+echo "Port 80 is already forwarded to 8000"
+else
+echo "Forwarding port 80 to 8000"
+nohup sudo socat TCP-LISTEN:80,fork TCP:localhost:8000 &
+fi
