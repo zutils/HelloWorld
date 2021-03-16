9f67653ad76405878bc283465607cdf47592a9fe
James Prince <34577138+zutils@users.noreply.github.com>
2021-03-16 10:45:32 -0400

    Update ansible_configure_script.sh

--- ansible_configure_script.sh
+++ ansible_configure_script.sh
@@ -38,7 +38,7 @@ fi
 # Do rolling update!
 dockerpath=jzypo/udacity:hello-world-server
 minikube kubectl -- run hello-world-server --image $dockerpath --port=80
-minikube kubectl -- kubectl get pods
+minikube kubectl -- get pods
 
 # Run in background - forward port 80 to the server's port 8000
 #nohup minikube kubectl --  port-forward hello-world-server 80:8000
